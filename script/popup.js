3ca7e11589481818f4bca3c4cb9e5c8fe37f54f0
savin.arseni <savin.arseni@live.ru>
2017-10-04 22:22:17 +0600

    Process added for array tariffs

--- script/popup.js
+++ script/popup.js
@@ -2,62 +2,61 @@ window.onload = function () {
   var app = new Vue({
     el: '#app',
     data: {
-      titleComputed: '-',
-      value: '-',
-      percentage: '-',
       phoneNumber: '',
-      password: ''
+      password: '',
+      isSuccess: false,
+      tariffs: [{}]
     },
     computed: {
-      title: function () {
+      tariffsData: function () {
 
-        // Get data of store  
-        chrome.storage.sync.get({
-          phoneNumber: '',
-          password: ''
-        }, items => {
-          this.phoneNumber = items.phoneNumber;
-          this.password = items.password;
-        });
+        if (!this.isSuccess) {
+          // Get data of store  
+          chrome.storage.sync.get({
+            phoneNumber: '',
+            password: ''
+          }, items => {
+            this.phoneNumber = items.phoneNumber;
+            this.password = items.password;
+          });
 
-        if (this.phoneNumber != '' && this.password != '') {
-          this.$http.post('https://cabinet.altel.kz/login',
-            'loginId=' + this.phoneNumber + '&password=' + this.password + '',
-            {
-              emulateJSON: true,
-              method: 'POST',
-              credentials: true,
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+          if (this.phoneNumber != '' && this.password != '') {
+            this.$http.post('https://cabinet.altel.kz/login',
+              'loginId=' + this.phoneNumber + '&password=' + this.password + '',
+              {
+                emulateJSON: true,
+                method: 'POST',
+                credentials: true,
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                }
               }
-            }
-          ).then(response => {
-          }, response => {
-          });
+            ).then(response => {
+            }, response => {
+            });
 
-          this.$http.get('https://cabinet.altel.kz/getTariffResources',
-            {
-              emulateJSON: true,
-              method: 'GET',
-              responseType: 'json',
-              credentials: true
-            }
-          ).then(response => {
-            //this.title = response.data[0].title;
-            this.titleComputed = response.data[0].title;
-            this.value = response.data[0].value;
-            this.percentage = Math.round(response.data[0].percentage, 2) + '%';
-
-          }, response => {
-          });
-        } else {
-          console.log("Not set phone or password in options.");
+            this.$http.get('https://cabinet.altel.kz/getTariffResources',
+              {
+                emulateJSON: true,
+                method: 'GET',
+                responseType: 'json',
+                credentials: true
+              }
+            ).then(response => {
+              this.tariffs = response.data;
+              this.isSuccess = true;
+            }, response => {
+            });
+          } else {
+            console.log("Don't forget set authentication data.");
+          }
         }
-        return this.titleComputed;
+        return this.tariffs;
       }
     }
   })
 }
+
 function GetStorage(result) {
   chrome.storage.sync.get({
     phoneNumber: '',
